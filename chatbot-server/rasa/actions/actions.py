e1a66ed1331e2abbbc25f7a2811d440d153a9980
Chử Văn Hà <chuvanha1607@gmail.com>
2025-05-16 15:18:58 +0700

    Cập nhật 7
    Hoàn thành huấn luyện dữ liệu về các môn học khóa 14

--- chatbot-server/rasa/actions/actions.py
+++ chatbot-server/rasa/actions/actions.py
@@ -22,7 +22,214 @@ MON_HOC_INFO = {
         "số tín chỉ": "3 (1, 0, 0, 2)",
         "giảng viên": "Lê Trung Thực, Phạm Thị Loan...",
         "mô tả": "Sinh viên tham gia thực tập tại doanh nghiệp, học cách ứng dụng kỹ năng thực tế và viết báo cáo thu hoạch theo yêu cầu."
-    }
+    },
+     "Toán rời rạc": {
+        "mã học phần": "MI1213",
+        "số tín chỉ": "2 (2,0,0,0)",
+        "giảng viên": "Lê Mai Nam, Trần Xuân Thanh, Nguyễn Thị Nga..",
+        "mô tả": "Sinh viên sau khi hoàn thành học phần này sẽ: Có khả năng phân tích chia nhỏ bài toán thực tế lớn thành các bài toán thực tế nhỏ hơn. Chuyển các bài toán thực tế thành những bài toán toán học dưới dạng công thức. Sử dụng thuật toán để giải quyết các bài toán."
+    },
+         "Kiểm thử phần mềm": {
+        "mã học phần": "IT3240",
+        "số tín chỉ": "3 (2,1,0,0)   ",
+        "giảng viên": "Đoàn Thị Thuỳ Linh, Nguyễn Thị Nga, Lưu Thị Thảo,...",
+        "mô tả": "Học phần Kiểm thử phần mềm là học phần thuộc khối kiến thức chuyên ngành Công nghệ phần mềm. Học phần này cung cấp cho sinh viên kiến thức về kiểm thử phần mềm, các quy trình kiểm thử phần mềm, các kỹ thuật cơ bản trong phân tích và thiết kế test case, thực hiện kiểm thử và báo cáo kết quả kiểm thử. Ngoài ra, học phần này cũng cung cấp sự hiểu biết và cách sử dụng một số công cụ hỗ trợ quản lý lỗi, một số công cụ hỗ trợ kiểm thử tự động. Bên cạnh đó, sinh viên được làm việc trong các nhóm và thuyết trình các vấn đề nâng cao sử dụng các phương tiện trình chiếu. Sau khi học xong học phần này sinh viên có khả năng kiểm thử và đánh giá chất lượng một phần mềm cụ thể trong thực tế."
+    },
+         "Cơ sở lập trình với C": {
+        "mã học phần": "IT2201",
+        "số tín chỉ": "3 (2,1,0,0)   ",
+        "giảng viên": "Mai Văn Linh, Lưu Thị Thảo",
+        "mô tả": "Học phần Cơ sở lập trình với C là học phần thuộc khối kiến thức cơ sở ngành của ngành Công nghệ thông tin. Học phần giúp cho sinh viên hiểu rõ các khái niệm cơ bản về thuật toán (giải thuật) và các cấu trúc chương trình. Các khái niệm cơ bản; các kiểu dữ liệu; các câu lệnh vào - ra dữ liệu; các cấu trúc điều khiển; hàm; con trỏ; kiểu cấu trúc trong ngôn ngữ C. Sau khi học xong học phần này người học có khả năng sử dụng được các kĩ thuật lập trình và ngôn ngữ lập trình C để lập trình những bài toán cơ bản, đơn giản."
+    },
+         "Kỹ thuật đồ họa máy tính": {
+        "mã học phần": "IT13236",
+        "số tín chỉ": "3 (2, 1, 0, 0)",
+        "giảng viên": "Ngô Thùy Giang, Nguyễn Hải Bình, Lê Mai Nam,...",
+        "mô tả": "Học phần Kỹ thuật đồ họa máy tính là học phần thuộc khối kiến thức chuyên ngành của ngành Công nghệ thông tin. Học phần cung cấp cho sinh viên những kiến thức tổng quan về đồ hoạ máy tính, các phương pháp, giải thuật kiến tạo đồ hoạ. Sau khi học xong học phần này người học có khả năng:- Hiểu biết về các ứng dụng thực tế của đồ họa máy tính trong các lĩnh vực cuộc sống.- Nắm được các phương pháp, thành phần, nguyên lý của giải thuật tạo dựng và xử lý một đối tượng đồ hoạ trên máy tính.- Xây dựng các đối tượng đồ hoạ dựa trên các thuật toán.- Thiết kế các hình ảnh đồ hoạ với thư viện đồ họa OpenGL và ngôn ngữ lập trình C++.- Giải quyết các bài toán đồ hoạ ứng dụng trong việc mô phỏng thế giới thực."
+    },
+         "Thiết kế web": {
+        "mã học phần": "IT2203",
+        "số tín chỉ": "3 (2,1,0,0)  ",
+        "giảng viên": "Ngô Thùy Giang, Trần Nguyên Hoàng, Đỗ Thị Huyền ",
+        "mô tả": "Học phần Thiết kế web là học phần thuộc khối kiến thức cơ sở ngành Công nghệ phần mềm. Học phần mô tả những kiến thức cơ bản và nâng cao về thiết kế web:- Giới thiệu các kiến thức cơ bản về internet, web; những khái niệm cơ bản về cấu trúc một website và các nguyên tắc thiết kế web.- Cung cấp những kiến thức cơ bản trong việc thiết kế website sử dụng công nghệ  HTML5, CSS3, Javascript- Tiếp cận các kiến thức cơ bản và nâng cao về thiết kế layout, sử dụng những kĩ thuật tiên tiến nhất của HTML5, CSS3 và kỹ thuật lập trình Javascript."
+    },
+         "Công nghệ Java": {
+        "mã học phần": "IT3242",
+        "số tín chỉ": "4 (3,1,0,0)",
+        "giảng viên": "Trần Xuân Thanh, Trần Nguyên Hoàng, Nguyễn Đức Thiện,...",
+        "mô tả": "Học phần Công nghệ Java là học phần thuộc khối kiến thức chuyên ngành. Học phần giúp sinh viên hiểu rõ kiến thức về các công nghệ trong hệ sinh thái Java. Định hướng cho sinh viên trong việc thiết kế và phát triển các sản phẩm phần mềm, webstie trên nền tảng công nghệ Java. Cung cấp cho sinh viên kiến thức cơ bản về ngôn ngữ lập trình Java. Giúp sinh viên có khả năng thiết kế, phát triển và bảo trì các ứng dụng Java độc lập và ứng dụng web. Nâng cao kỹ năng tư duy logic và giải quyết vấn đề cho sinh viên. Rèn luyện kỹ năng làm việc độc lập và làm việc nhóm cho sinh viên"
+    },
+         "Phát triển ứng dụng cho thiết bị di động": {
+        "mã học phần": "IT2205",
+        "số tín chỉ": "4 (3,1,0,0)",
+        "giảng viên": "Trần Xuân Thanh, Trần Nguyên Hoàng, Nguyễn Đức Thiện,...",
+        "mô tả": "Học phần Phát triển ứng dụng cho thiết bị di động là học phần thuộc khối kiến thức chuyên ngành. Học phần giúp sinh viên hiểu rõ về cấu trúc hệ điều hành Android và các bước phát triển một ứng dung trên nên tảng Android cho các thiết bị di động."
+    },
+         "Lập trình web với PHP": {
+        "mã học phần": "IT3220",
+        "số tín chỉ": "4 (3,1,0,0)  ",
+        "giảng viên": "Mai Văn Linh, Nguyễn Đức Thiện (84), Đỗ Thị Huyền,...",
+        "mô tả": "Học phần Lập trình web với PHP là học phần thuộc khối kiến thức chuyên ngành, học phần giúp học sinh hiểu rõ các bước xây dựng một web site, sử dụng thành thạo được ngôn ngữ lập trình PHP để tạo ra một ứng dụng website và triển khai trên môi trường trực tuyến. Về kiến thức: Hiểu được các nguyên lý về thiết kế  Web với HTML, định dạng website với CSS, Javascript; Có kiến thức về ngôn ngữ lập trình PHP. Về kỹ năng: Thành thạo cơ bản lập trình website quản lý bán hàng trên website với csdl mysql. Năng lực tự chủ và trách nhiệm: Nghiêm túc, trách nhiệm, chủ động, tích cực, chăm chỉ, cẩn thận và cần có thái độ tự nghiên cứu học hỏi cao."
+    },
+         "Cấu trúc dữ liệu và giải thuật": {
+        "mã học phần": "IT2207",
+        "số tín chỉ": "3 (2,1,0,0)  ",
+        "giảng viên": "Trần Nguyên Hoàng",
+        "mô tả": "Học phần được giảng dạy năm thứ 2 giúp sinh viên thực sự hiểu được tầm quan trọng của giải thuật và cấu trúc dữ liệu - hai thành tố quan trọng của một chương trình; các kiểu cấu trúc dữ liệu thông dụng và các giải thuật trên các cấu trúc dữ liệu ấy."
+    },
+         "Công nghệ phần mềm": {
+        "mã học phần": "IT3226",
+        "số tín chỉ": "2 (A,B,C,D)  ",
+        "giảng viên": "",
+        "mô tả": "Học phần Công nghệ phần mềm là học phần thuộc khối kiến thức chuyên ngành Công nghệ phần mềm. Học phần giúp sinh viên nắm được những Các nguyên lý cơ bản trong kỹ thuật phần mềm trên ba lĩnh vực yêu cầu, thiết kế và kiểm tra; Kỹ thuật phân tích dựa trên sơ đồ dòng dữ liệu (DFD); Các phân tích hướng đối tượng sử dụng UML; Các mô hình phát triển phần mềm; Kiểm tra đánh giá hệ thống; Quản trị và ước lượng dự án. "
+    },
+         "Phương pháp tính toán tối ưu": {
+        "mã học phần": "IT2209",
+        "số tín chỉ": "2 (2,0,0,0)",
+        "giảng viên": "Lê Mai Nam, Lê Trung Thực, Trần Xuân Thanh,...",
+        "mô tả": "Mục tiêu: Sau khi hoàn thành học phần này, sinh viên có khả năng: Nắm được các thuật toán giải quyết các bài toán quy hoạch, bài toán vận tải, bài toán quy hoạch động và một số mô hình lý thuyết điều khiển dự trữ. Cài đặt được các thuật toán đề cập trong phần trên. Đánh giá các thuật toán, kỹ thuật giải quyết bài toán kể trên. Thiết kế được các thuật toán, giải pháp, để giải quyết các bài toán liên quan. Giải quyết các bài toán dựa trên cơ sở các bài toán đã giải quyết được liệt kê trong phần trên. Nội dung: Học phần cung cấp cho sinh viên những kiến thức tổng quan về phương pháp tính toán tối ưu, các giải thuật giải quyết các bài toán. "
+    },
+         "Lập trình hướng đối tượng với Java": {
+        "mã học phần": "IT3219",
+        "số tín chỉ": "3 (2,1,0,0) ",
+        "giảng viên": "Trần Nguyên Hoàng",
+        "mô tả": "Cung cấp cho sinh viên kiến thức về phương pháp lập trình hướng đối tượng, một phương pháp rất phổ biến hiện nay. Định hướng cho sinh viên trong việc thiết kế một chương trình theo phương pháp hướng đối tượng, sử dụng các khái niệm như kiểu dữ liệu trừu tượng, nguyên tắc kế thừa trong việc phát triển các kiểu dữ liệu, tính đa hình"
+    },
+         "Lập trình .NET": {
+        "mã học phần": "IT3212",
+        "số tín chỉ": "4 (2,2,0,0)  ",
+        "giảng viên": "Trần Nguyên Hoàng",
+        "mô tả": "Học phần được giảng dạy năm thứ 2 giúp sinh viên thực sự hiểu được tầm quan trọng của giải thuật và cấu trúc dữ liệu - hai thành tố quan trọng của một chương trình; các kiểu cấu trúc dữ liệu thông dụng và các giải thuật trên các cấu trúc dữ liệu ấy."
+    },
+         "Thực tập nhận thức ngành nghề": {
+        "mã học phần": "IT2208",
+        "số tín chỉ": "2 (2,0,0,0)",
+        "giảng viên": "Nguyễn Thị Thúy Nga",
+        "mô tả": "Nhận thức rõ về chương trình đào tạo ngành CNTT trường ĐHCNĐA. Nhận thức tốt vể nhập môn ngành CNTT trường ĐHCNĐA. Hình thành nhận thức cơ bản về môi trường làm việc, lĩnh vực hoạt động của ngành công nghệ thông tin, nền tảng cho tác phong quy cách làm việc và một số kỹ năng làm việc cơ bản, như: liên quan tới CNTT, cách thức biểu diễn dữ liệu trong máy tính và hệ đếm, tổng quan về lập trình; Cơ sở dữ liệu, quản trị dữ liệu và  Hệ thống thông tin quản lý.  Xây dựng các mối quan hệ trong công việc tại các tổ chức, doanh nghiệp. Rèn luyện tác phong làm việc, kỹ năng, năng lực làm việc chuyên nghiệp, độc lập và sáng tạo. Định hướng nghề nghiệp, định hướng kế hoạch học tập và phát triển bản thân."
+    },
+         "Quản lý dự án CNTT": {
+        "mã học phần": "IT3230",
+        "số tín chỉ": "2 (2,0,0,0)   ",
+        "giảng viên": "",
+        "mô tả": "Học phần Quản lý dự án CNTT là học phần thuộc khối kiến thức chuyên ngành Công nghệ phần mềm. Nội dung học phần bao gồm kiến thức cơ bản về Quản lý dự án Công nghệ Thông tin (QLDA CNTT) như: Dự án, Quản lý dự án CNTT, vai trò của người tham gia dự án, vòng đời của dự án, bối cảnh và xu hướng mới ảnh hưởng đến dự án CNTT. Trình bày về các giai đoạn quản lý dự án CNTT, các lĩnh vực hoạt động trong quản lý dự án như: QL tích hợp, QL phạm vi, QL thời gian, QL chi phí, QL chất lượng, QL nhân lực, QL thông tin và truyền thông."
+    },
+         "Tiếng anh chuyên ngành công nghệ phần mềm": {
+        "mã học phần": "IT3239",
+        "số tín chỉ": "2(2,0,0,0)",
+        "giảng viên": "",
+        "mô tả": "The module provides the following knowledge and skills: Understanding of the field of software engineering in general and application software in particular. Basic knowledge of programming languages, classification of programming languages ​​in the information technology industry. Basic computer networks and the Internet. Provides an overview of IT industry jobs and the people who work in. This module presents topics about three main areas in Information Technology: Computer Software, Networking and IT Careers."
+    },
+             "Đồ án chuyên ngành công nghệ phần mềm": {
+        "mã học phần": "IT3228",
+        "số tín chỉ": "5 (1, 0, 4, 0)",
+        "giảng viên": "Đỗ Thị Huyền, Lê Trung Thực, Nguyễn Thị Thúy Nga,...",
+        "mô tả": "Học phần Đồ án chuyên ngành công nghệ phần mềm là học phần thuộc khối kiến thức chuyên ngành Công nghệ phần mềm. Học phần giúp người học vận dụng được các kiến thức đã học trong phân tích thiết kế hệ thống; phân tích, thiết kế CSDL quan hệ; các ngôn ngữ lập trình giải quyết các yêu cầu của bài toán trong thực tế. Sau khi học xong học phần này người học có khả năng Phân tích và thiết kế một hệ thống trong thực tế, đưa ra được giải pháp về nghiệp vụ và lựa chọn được công nghệ phù hợp để giải quyết được các yêu cầu của bài toán đã đặt ra. Viết được báo cáo theo đúng mẫu và đầy đủ yêu cầu của Đồ án chuyên ngành công nghệ phần mềm. Thuyết trình được báo cáo trước lớp và giảng viên chấm báo cáo. "
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+             "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+         "": {
+        "mã học phần": "",
+        "số tín chỉ": "",
+        "giảng viên": "",
+        "mô tả": ""
+    },
+    
+    
+
 }
 #7. sau khi có dữ liệu các môn học thì cần lớp xử lí các dữ liệu trên như lớp dưới đây 
 class ActionTraCuuThongTinMonHoc(Action):
